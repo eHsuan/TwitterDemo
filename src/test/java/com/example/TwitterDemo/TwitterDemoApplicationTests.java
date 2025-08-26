ede5cd0477699183f3c6e4faf2977d41af286bfd
eHsuan <ac131151@gmail.com>
2025-08-26 21:20:44 +0800

    test

--- src/test/java/com/example/TwitterDemo/TwitterDemoApplicationTests.java
+++ src/test/java/com/example/TwitterDemo/TwitterDemoApplicationTests.java
@@ -1,9 +1,7 @@
 package com.example.TwitterDemo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class TwitterDemoApplicationTests {
 
 	@Test
